0cbea29135121c167d4bd173e851117c12d88078
Axel <axosar@gmail.com>
2019-07-08 11:28:29 -0500

    First Test
    Testing googleSearch

--- script.test.js
+++ script.test.js
@@ -1,3 +1,18 @@
-it('this is a test', () => {
-  
+const googleSearch = require('./script');
+
+dbMock = [
+  'dog.com',
+  'cheesepuf.com',
+  'disney.com',
+  'dogpictures.com'
+]
+
+it('it is a silly test', () => {
+  expect('Hello').toBe('Hello');
+  //googleSearch('testtest', dbMock);
+});
+
+it('is searching google', () => {
+  expect(googleSearch('testtest', dbMock)).toEqual([])
+  expect(googleSearch('dog', dbMock)).toEqual(['dog.com','dogpictures.com'])
 })
